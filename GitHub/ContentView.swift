0db30f2364c9795fb6374efbadfab68a6a8ef41d
sirop4iq <serhiott@gmail.com>
2024-09-04 16:39:12 +0300

    fix code

--- GitHub/ContentView.swift
+++ GitHub/ContentView.swift
@@ -16,6 +16,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            Text("Fix code!")
         }
         Spacer()
         .padding()
