fb16278c4af57824e18c5163026352aeac03bbb6
sirop4iq <serhiott@gmail.com>
2024-09-04 16:21:06 +0300

    necommit

--- GitHub/ContentView.swift
+++ GitHub/ContentView.swift
@@ -10,15 +10,18 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-       
+            Spacer()
 
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
         }
-        .padding()
         Spacer()
+        .padding()
+  
+       
+
     }
 }
 
