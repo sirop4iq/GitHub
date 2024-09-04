11574f3e579720c1c830280c30bf121a2b7555e8
sirop4iq <serhiott@gmail.com>
2024-09-04 10:53:09 +0300

    This my commit

--- GitHub/ContentView.swift
+++ GitHub/ContentView.swift
@@ -10,7 +10,9 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-           
+            Button("Open GitHub") {
+                
+            }
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
