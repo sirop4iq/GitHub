774a73482f6afb38976f43d61c9c3eb1cc93a479
sirop4iq <serhiott@gmail.com>
2024-09-04 11:02:03 +0300

    Second commit

--- GitHub/ContentView.swift
+++ GitHub/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 
             }
            Text("First commit")
+            Text("Secondz commit")
 
             Button("Open GitHub") {
                 
