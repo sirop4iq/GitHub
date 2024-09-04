08b282b972400064ccea6f64ff2e27d7712ac2ca
sirop4iq <serhiott@gmail.com>
2024-09-04 10:52:17 +0300

    Initial Commit

---
+++ GitHub/GitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubApp.swift
+//  GitHub
+//
+//  Created by Сергей Быкадоров on 04.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct GitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
